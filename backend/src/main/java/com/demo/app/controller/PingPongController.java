dc6d9c191fc8c850c44a056263d6d433f0b9abe4
Vikas Bagri <bagrivikas1998@gmail.com>
2024-05-25 14:11:42 +0530

    add ping-pong controller to test continuous delivery workflow

---
+++ backend/src/main/java/com/demo/app/controller/PingPongController.java
@@ -0,0 +1,16 @@
+package com.demo.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    record PingPong(String result) {}
+
+    @GetMapping("/ping")
+    public PingPong pingPong() {
+        return new PingPong("pong :(");
+    }
+
+}
